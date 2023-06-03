af7ee111aba588b49e149aba375b809e0e41c261
Akshit Kathuria <akshitkathuria17@gmail.com>
2023-06-03 12:23:56 +0530

    Added many more features

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Packet_Sniffer
+
+# Packet Sniffer
+
+This is a packet sniffing script written in Python using the Scapy library. It allows you to capture and analyze network packets on a specified interface. The script provides options to filter packets based on a filter expression and keyword, and it saves the captured packets to a log file.
+
+## Features
+
+- Capture network packets on a specified interface.
+- Filter packets based on a filter expression in BPF syntax.
+- Filter packets based on a keyword in the payload.
+- Limit the number of packets captured.
+- Save captured packets and relevant information to a log file.
+
+## Requirements
+
+- Python 3.x
+- Scapy library (`pip install scapy`)
+
+**Note:**
+
+- When running the script, you might need to use `sudo` or run the script as an administrator, depending on your operating system and network interface permissions. This is required to have the necessary permissions for capturing network packets.
+
+## Usage
+
+```
+python sniff.py [-h] [-i INTERFACE] [-e EXPRESSION] [-f FILTER] [-l LOG] [-c COUNT]
+```
+
+### Options
+
+- `-h, --help`: Show the help message and usage information.
+- `-i INTERFACE, --interface INTERFACE`: Specify the interface to sniff on. Default: eth0.
+- `-e EXPRESSION, --expression EXPRESSION`: Specify the filter expression in BPF syntax (optional).
+- `-f FILTER, --filter FILTER`: Filter keyword to limit the sniffing (optional).
+- `-l LOG, --log LOG`: Specify the log file to save the captured packets. Default: sniff_log.txt.
+- `-c COUNT, --count COUNT`: Specify the maximum number of packets to capture (optional).
+
+## Examples
+
+1. Capture packets on the default interface (eth0) and save to the default log file (sniff_log.txt):
+```
+python sniff.py
+```
+
+2. Capture packets on a specific interface (e.g., wlan0) and save to a specific log file (capture.log):
+```
+python sniff.py -i wlan0 -l capture.log
+```
+
+3. Capture HTTP packets with a filter expression and filter for a specific keyword:
+```
+python sniff.py -e "tcp port 80" -f password
+```
+
+## Notes
+
+- Make sure to have the necessary permissions and comply with legal and ethical considerations when capturing and analyzing network traffic.
+- Depending on your operating system and network interface permissions, you might need to run the script with `sudo` or as an administrator to have the necessary permissions for capturing network packets.
+
--- sniffer.py
+++ sniffer.py
@@ -1,31 +1,60 @@
 import scapy.all as scapy
 from scapy.layers import http
-def sniff(interface):
-    scapy.sniff(iface=interface,store=False,prn=process_sniffed_packet)
-    
+import argparse
+
+def sniff(interface, filter_expression, filter_keyword, log_file, count):
+    captured_packets = []
+
+    def process_sniffed_packet(packet):
+        if packet.haslayer(http.HTTPRequest):
+            captured_packets.append(packet)
+            if count and len(captured_packets) >= count:
+                return True
+
+    scapy.sniff(iface=interface, filter=filter_expression, store=False, prn=process_sniffed_packet, stop_filter=lambda _: len(captured_packets) >= count if count else False)
+    analyze_packets(captured_packets, filter_keyword, log_file)
 
 def get_url(packet):
-    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path if packet.haslayer(http.HTTPRequest) else ""
 
 def get_login_info(packet):
     if packet.haslayer(scapy.Raw):
-        load=packet[scapy.Raw].load
-        keywords=["username","user","login","admin","password","pass"]
-        for keyword in keywords:
-            if keyword in load:
-                return load
-            
-   
-   
-            
-def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        url=get_url(packet)
-        print("[+] HTTP Req >>"+ url)
-        
-        login_info=get_login_info(packet)
-        if login_info:
-            print("\n\n [+] Possible username/pass "+ login_info+ "\n\n")
-            
-            
-sniff("eth0")
+        try:
+            load = packet[scapy.Raw].load.decode('utf-8', 'ignore')
+            keywords = ["username", "user", "login", "admin", "password", "pass"]
+            for keyword in keywords:
+                if keyword in load:
+                    return load
+        except UnicodeDecodeError:
+            pass
+    return None
+
+def analyze_packets(captured_packets, filter_keyword, log_file):
+    with open(log_file, "a") as f:
+        f.write("Captured Packets:\n")
+        for packet in captured_packets:
+            url = get_url(packet)
+            login_info = get_login_info(packet)
+            if login_info and (not filter_keyword or filter_keyword in login_info):
+                f.write("[+] HTTP Request >> " + url + "\n")
+                f.write("[+] Possible username/password >> " + login_info + "\n\n")
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Specify the interface to sniff on")
+    parser.add_argument("-e", "--expression", dest="expression", help="Specify the filter expression in BPF syntax")
+    parser.add_argument("-f", "--filter", dest="filter", help="Filter keyword to limit the sniffing")
+    parser.add_argument("-l", "--log", dest="log", help="Specify the log file")
+    parser.add_argument("-c", "--count", dest="count", type=int, help="Specify the maximum number of packets to capture")
+    options = parser.parse_args()
+    return options
+
+if __name__ == "__main__":
+    options = get_arguments()
+    interface = options.interface if options.interface else "eth0"
+    filter_expression = options.expression if options.expression else ""
+    filter_keyword = options.filter
+    log_file = options.log if options.log else "sniff_log.txt"
+    count = options.count
+
+    sniff(interface, filter_expression, filter_keyword, log_file, count)
